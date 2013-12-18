b4ac31b0ec6b00516fdc215aa8f942b588f73dc8
David Adler <dalberto.adler@gmail.com>
2013-12-18 15:30:16 -0200

    HACKY fix for static, temporary commit

--- starter/urls.py
+++ starter/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # from django.contrib import admin
 # admin.autodiscover()
@@ -7,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^$', 'core.views.home', name='home'),
     url(r'^list_static/?', 'core.views.list_static', name='list_static'),
     # url(r'^hellodjango/', include('hellodjango.foo.urls')),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -14,3 +16,4 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
